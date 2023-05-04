3b26348b51b7a6004f2685b690036b8aa419a2b3
mmabdullah05 <100152133+mmabdullah05@users.noreply.github.com>
2023-05-04 22:31:57 +0500

    file to 7zip to base64 and vice versa.

---
+++ src-tauri/src/zip7_base64.rs
@@ -0,0 +1,33 @@
+use std::fs;
+use std::process::Command;
+use base64::{encode};
+use base64::{decode};
+
+fn front_FiletoString() {
+sevenz_rust::compress_to_path("Create boids simulation in 2d, simi.txt",
+"Create boids simulation in 2d, simi(7z-type).7z")
+.expect("compress ok");
+
+// convert to base64String 
+                    let file_data = fs::read("Create boids simulation in 2d, simi(7z-type).7z").expect("failed to read temporary file");
+
+                    // Encode the file data as a Base64 string
+                    let base64_string = encode(&file_data);
+
+                    // Print the Base64 string to the console
+                    println!("Base64 string: {}", base64_string);
+
+}
+
+fn back_StringtoFile(){
+//  convert from base64 String
+
+let file_data = decode(base64_string).unwrap();
+
+// Write the binary data to a file
+fs::write("Create boids simulation in 2d, simi(converted-back).7z", &file_data).expect("failed to write file");
+
+sevenz_rust::decompress_file("Create boids simulation in 2d, simi(converted-back).7z", "Dan-done.txt").expect("complete");
+}
+
+
