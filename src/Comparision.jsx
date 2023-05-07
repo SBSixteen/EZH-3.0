3d7d4bd17e56b6c62ddd450b129c5a5837f74e56
Fateh <m.fateh.surahio@gmail.com>
2023-05-07 21:48:41 +0500

    comparission alignment issue

--- src/Comparision.jsx
+++ src/Comparision.jsx
@@ -13,6 +13,7 @@ function Comparison() {
           { name: 'Salary', value: 5000 },
           { name: 'B2B', value: 2000 },
           { name: 'Investments', value: 10000 },
+          { name: 'abbakapaisa', value: 500 },
         ],
       },
       {
@@ -22,7 +23,6 @@ function Comparison() {
         income: [
           { name: 'Salary', value: 7000 },
           { name: 'Bonus', value: 1500 },
-          { name: 'Dividend', value: 5000 },
         ],
       },
       {
@@ -322,11 +322,13 @@ function Comparison() {
               'rgba(255, 99, 132, 0.2)',
               'rgba(54, 162, 235, 0.2)',
               'rgba(255, 206, 86, 0.2)',
+              'rgba(23, 106, 44, 0.2)',
             ],
             borderColor: [
               'rgba(255, 99, 132, 1)',
               'rgba(54, 162, 235, 1)',
               'rgba(255, 206, 86, 1)',
+              'rgba(23, 106, 44, 1)',
             ],
             borderWidth: 1,
           },
@@ -349,11 +351,13 @@ function Comparison() {
               'rgba(255, 99, 132, 0.2)',
               'rgba(54, 162, 235, 0.2)',
               'rgba(255, 206, 86, 0.2)',
+              'rgba(23, 206, 44, 0.2)',
             ],
             borderColor: [
               'rgba(255, 99, 132, 1)',
               'rgba(54, 162, 235, 1)',
               'rgba(255, 206, 86, 1)',
+              'rgba(23, 106, 44, 1)',
             ],
             borderWidth: 1,
           },
@@ -377,19 +381,43 @@ function Comparison() {
 }
 
 return (
-        <><div className="tabs-div">
-        {users.map((user, index) => (
-          <button
-          key={index}
-          className={`my-button ${activeSheet === index ? 'active' : ''}`}
-          onClick={() => handleTabClick(index)}
-        >
-          {user.name}
-        </button>
-        
-        ))}
-      </div>
-      <div className="user-info">
+  <><div className="row">
+    <a href="" target="_blank">
+      <img
+        src="/Logo_Ezhire.svg"
+        className="logo tauri"
+        alt="Tauri logo" />
+    </a>
+  </div><><div className="tabs-div">
+    {users.map((user, index) => (
+      <button
+        key={index}
+        className={`my-button ${activeSheet === index ? 'active' : ''}`}
+        onClick={() => handleTabClick(index)}
+      >
+        {user.name}
+      </button>
+
+    ))}
+  </div>
+
+  <div  style={{ height: "50px" }}>
+          <div style={{ width: "45%", float: "left" ,  display:"flex",marginLeft:"2.5%", marginTop:"10px"}}>
+          <p>Name: {users[activeSheet].name}</p>
+          <br></br>
+        <p>Age: {users[activeSheet].age}</p>
+        <br></br>
+        <p>Email: {users[activeSheet].email}</p></div>
+        <br></br>
+          <div className="charts-pie" style={{  float: "right" , display:"flex", marginRight:"2.5%", justifyContent:"flex-end"}}>
+          <canvas ref={pieChartRef} />   
+          </div>
+          <br></br>
+          <div className="charts-bar" style={{  float: "right" , display:"flex", marginRight:"2.5%", justifyContent:"flex-end"}}>
+          <canvas ref={barChartRef} />   
+          </div>
+        </div>
+      {/* <div className="user-info">
         <p>Name: {users[activeSheet].name}</p>
         <p>Age: {users[activeSheet].age}</p>
         <p>Email: {users[activeSheet].email}</p>
@@ -399,8 +427,8 @@ return (
       </div>
       <div className="charts-bar">
         <canvas ref={barChartRef} />
-      </div>
-      </>
+      </div> */}
+    </></>
 );
 }
 
@@ -525,367 +553,3 @@ export default Comparison;
 
 // export default Comparison;
 
-//greatest//
-
-
-// import React, { useState, useRef, useEffect } from 'react';
-// import Chart from 'chart.js/auto';
-// import "./style.css";
-
-// function Comparison() {
-//   const userData = [    {      name: 'Alice',      age: 1,      email: 'a',      income: [        { name: 'Salary', value: 5000 },        { name: 'Investments', value: 2000 },        { name: 'Rent', value: 1000 }      ]
-//     },
-//     {
-//       name: 'Bob',
-//       age: 30,
-//       email: 'bob@example.com',
-//       income: [
-//         { name: 'Salary', value: 6000 },
-//         { name: 'Investments', value: 3000 },
-//         { name: 'Rent', value: 2000 }
-//       ]
-//     },
-//     {
-//       name: 'Charlie',
-//       age: 35,
-//       email: 'charlie@example.com',
-//       income: [
-//         { name: 'Salary', value: 4000 },
-//         { name: 'Investments', value: 1000 },
-//         { name: 'Rent', value: 1500 }
-//       ]
-//     },
-//   ];
-
-//   const [activeSheet, setActiveSheet] = useState(1);
-//   const [activeIncome, setActiveIncome] = useState(userData[0].income[0].name);
-//   const [chartData, setChartData] = useState(null);
-//   const chartRef = useRef(null);
-
-//   useEffect(() => {
-//     const incomeData = userData[activeSheet - 1].income.find(income => income.name === activeIncome);
-//     const data = {
-//       labels: userData[activeSheet - 1].income.map(income => income.name),
-//       datasets: [
-//         {
-//           data: userData[activeSheet - 1].income.map(income => income.value),
-//           backgroundColor: [
-//             'rgba(255, 99, 132, 0.2)',
-//             'rgba(54, 162, 235, 0.2)',
-//             'rgba(255, 206, 86, 0.2)',
-//           ],
-//           borderColor: [
-//             'rgba(255, 99, 132, 1)',
-//             'rgba(54, 162, 235, 1)',
-//             'rgba(255, 206, 86, 1)',
-//           ],
-//           borderWidth: 1,
-//         },
-//       ],
-//     };
-//     setChartData(data);
-//   }, [activeSheet, activeIncome]);
-
-//   function handleTabClick(sheetNumber) {
-//     setActiveSheet(sheetNumber);
-//     setActiveIncome(userData[sheetNumber - 1].income[0].name);
-//   }
-
-//   function handleIncomeClick(incomeName) {
-//     setActiveIncome(incomeName);
-//   }
-
-//   useEffect(() => {
-//     if (chartRef.current && chartData) {
-//       const chartInstance = new Chart(chartRef.current, {
-//         type: 'pie',
-//         data: chartData,
-//         options: {},
-//       });
-//       return () => {
-//         chartInstance.destroy();
-//       };
-//     }
-//   }, [chartData]);
-
-//   return (
-//     <>
-//       <div className="tabs-div">
-//         <div className="tabs">
-//           {userData.map((user, index) => (
-//             <button
-//               key={index}
-//               className={activeSheet === index + 1 ? 'active' : ''}
-//               onClick={() => handleTabClick(index + 1)}
-//             >
-//               {user.name}
-//             </button>
-//           ))}
-//         </div>
-//         <div className="income-tabs">
-//           {userData[activeSheet - 1].income.map((income, index) => (
-//                     <button
-//                     key={index}
-//                     className={activeIncome === income.name ? 'active' : ''}
-//                     onClick={() => handleIncomeClick(income.name)}
-//                   >
-//                     {income.name}
-//                   </button>
-//                 ))}
-//               </div>
-//             </div>
-//             <div className="chart-container">
-//               <canvas ref={chartRef} />
-//             </div>
-//           </>
-// );
-// }
-
-// export default Comparison;          
-
-
-/*gooood one*/
-// import React, { useState, useRef, useEffect } from 'react';
-// import Chart from 'chart.js/auto';
-// import "./style.css";
-
-// function Comparison() {
-//   const userData = [
-//     {
-//       name: 'Alice',
-//       age: 1,
-//       email: 'a',
-//     },
-//     {
-//       name: 'Bob',
-//       age: 30,
-//       email: 'bob@example.com',
-//     },
-//     {
-//       name: 'Charlie',
-//       age: 35,
-//       email: 'charlie@example.com',
-//     },
-//   ];
-
-//   const [activeSheet, setActiveSheet] = useState(1);
-//   const [chartData, setChartData] = useState({
-//     labels: ['Age', 'Email Length'],
-//     datasets: [
-//       {
-//         data: [25, 16],
-//         backgroundColor: [
-//           'rgba(255, 99, 132, 0.2)',
-//           'rgba(54, 162, 235, 0.2)',
-//         ],
-//         borderColor: [
-//           'rgba(255, 99, 132, 1)',
-//           'rgba(54, 162, 235, 1)',
-//         ],
-//         borderWidth: 1,
-//       },
-//     ],
-//   });
-
-//   const chartRef = useRef(null);
-
-//   useEffect(() => {
-//     if (chartRef.current && chartData.datasets) {
-//       const chartInstance = new Chart(chartRef.current, {
-//         type: 'pie',
-//         data: chartData,
-//         options: {},
-//       });
-//       return () => {
-//         chartInstance.destroy();
-//       };
-//     }
-//   }, [chartData]);
-
-//   function handleTabClick(sheetNumber) {
-//     setActiveSheet(sheetNumber);
-
-//     const data = {
-//       labels: ['Age', 'Email Length'],
-//       datasets: [
-//         {
-//           data: [userData[sheetNumber - 1].age, userData[sheetNumber - 1].email.length],
-//           backgroundColor: [
-//             'rgba(255, 99, 132, 0.2)',
-//             'rgba(54, 162, 235, 0.2)',
-//           ],
-//           borderColor: [
-//             'rgba(255, 99, 132, 1)',
-//             'rgba(54, 162, 235, 1)',
-//           ],
-//           borderWidth: 1,
-//         },
-//       ],
-//     };
-
-//     setChartData(data);
-//   }
-
-//   return (
-//     <>
-//       <div className="tabs-div">
-//         <div className="tabs">
-//           {userData.map((user, index) => (
-//             <button
-//               key={index}
-//               className={activeSheet === index + 1 ? 'active' : ''}
-//               onClick={() => handleTabClick(index + 1)}
-//             >
-//               {user.name}
-//             </button>
-//           ))}
-//         </div>
-//       </div>
-//       <div className="sheet">
-//         <h2>{userData[activeSheet - 1].name}</h2>
-//         <table>
-//           <thead>
-//             <tr>
-//               <th>Name</th>
-//               <th>Age</th>
-//               <th>Email</th>
-//             </tr>
-//           </thead>
-
-//           <tbody>
-//             <tr>
-//               <th>{userData[activeSheet - 1].name}</th>
-//               <th>{userData[activeSheet - 1].age}</th>
-//               <th>{userData[activeSheet - 1].email}</th>
-//             </tr>
-//           </tbody>
-//         </table>
-//       </div>
-//       <div className="chart-container">
-//         <canvas ref={chartRef} />
-//       </div>
-//     </>
-//   );
-// }
-
-// export default Comparison;
-
-/*gooooododododod one*/
-
-// import React, { useState } from 'react';
-// import Chart from 'chart.js/auto';
-// import "./style.css";
-
-// function Comparison() {
-//   const userData = [
-//     {
-//       name: 'Alice',
-//       age: 25,
-//       email: 'alice@example.com',
-//     },
-//     {
-//       name: 'Bob',
-//       age: 30,
-//       email: 'bob@example.com',
-//     },
-//     {
-//       name: 'Charlie',
-//       age: 35,
-//       email: 'charlie@example.com',
-//     },
-//   ];
-
-//   const [activeSheet, setActiveSheet] = useState(1);
-//   const [chartData, setChartData] = useState({
-//     labels: ['Age', 'Email Length'],
-//     datasets: [
-//       {
-//         data: [25, 16],
-//         backgroundColor: [
-//           'rgba(255, 99, 132, 0.2)',
-//           'rgba(54, 162, 235, 0.2)',
-//         ],
-//         borderColor: [
-//           'rgba(255, 99, 132, 1)',
-//           'rgba(54, 162, 235, 1)',
-//         ],
-//         borderWidth: 1,
-//       },
-//     ],
-//   });
-
-//   function handleTabClick(sheetNumber) {
-//     setActiveSheet(sheetNumber);
-
-//     const data = {
-//       labels: ['Age', 'Email Length'],
-//       datasets: [
-//         {
-//           data: [userData[sheetNumber - 1].age, userData[sheetNumber - 1].email.length],
-//           backgroundColor: [
-//             'rgba(255, 99, 132, 0.2)',
-//             'rgba(54, 162, 235, 0.2)',
-//           ],
-//           borderColor: [
-//             'rgba(255, 99, 132, 1)',
-//             'rgba(54, 162, 235, 1)',
-//           ],
-//           borderWidth: 1,
-//         },
-//       ],
-//     };
-
-//     setChartData(data);
-//   }
-
-//   return (
-//     <>
-//       <div className="tabs-div">
-//         <div className="tabs">
-//           {userData.map((user, index) => (
-//             <button
-//               key={index}
-//               className={activeSheet === index + 1 ? 'active' : ''}
-//               onClick={() => handleTabClick(index + 1)}
-//             >
-//               {user.name}
-//             </button>
-//           ))}
-//         </div>
-//       </div>
-//       <div className="sheet">
-//         <h2>{userData[activeSheet - 1].name}</h2>
-//         <table>
-//           <thead>
-//             <tr>
-//               <th>Name</th>
-//               <th>Age</th>
-//               <th>Email</th>
-//             </tr>
-//           </thead>
-
-//           <tbody>
-//             <tr>
-//               <th>{userData[activeSheet - 1].name}</th>
-//               <th>{userData[activeSheet - 1].age}</th>
-//               <th>{userData[activeSheet - 1].email}</th>
-//             </tr>
-//           </tbody>
-//         </table>
-//         <div style={{ width: '400px', height: '400px' }}>
-//           <canvas ref={(ref) => {
-//             if (ref && chartData.datasets) {
-//               new Chart(ref, {
-//                 type: 'pie',
-//                 data: chartData,
-//                 options: {},
-//               });
-//             }
-//           }} />
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default Comparison;
