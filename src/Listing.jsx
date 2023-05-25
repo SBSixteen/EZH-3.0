e9a503e452355fe88317c44b2e841e339cfd9c02
Ali <smalihaiderr@gmail.com>
2023-05-25 11:28:17 +0500

    as of yet

--- src/Listing.jsx
+++ src/Listing.jsx
@@ -1,59 +1,55 @@
-import './listing.css';
- 
+import "./listing.css";
 
-const data = 
-    [
-        {
-           "email":"gowtham@outlook.com",
-           "firstname":"gowtham",
-           "lastname":"ss",
-           "password":"outlook010"
-        },
-        {
-           "email":"ss@ss.com",
-           "firstname":"ss",
-           "lastname":"ss",
-           "password":"ss"
-        },
-        {
-           "email":"gow@gow.com",
-           "firstname":"gow",
-           "lastname":"gow",
-           "password":"gow"
-        }
-     ]
+const data = [
+  {
+    email: "gowtham@outlook.com",
+    firstname: "gowtham",
+    lastname: "ss",
+    password: "outlook010",
+  },
+  {
+    email: "ss@ss.com",
+    firstname: "ss",
+    lastname: "ss",
+    password: "ss",
+  },
+  {
+    email: "gow@gow.com",
+    firstname: "gow",
+    lastname: "gow",
+    password: "gow",
+  },
+];
 
- 
 function Listing() {
-    return (
-        <div className="Listing">
-       <table>
-  <thead>
-    <tr>
-      <th>First name</th>
-      <th>Last name</th>
-      <th>Password</th>
-      <th>Email</th>
-    </tr>
-  </thead>
-  <tbody>
-    {data.map(item => {
-      return (
-        <tr key={item.password}>
-          <td>{ item.firstname }</td>
-          <td>{ item.lastname }</td>
-          <td>{ item.password }</td>
-          <td>{ item.email }</td>
-          <button>Download</button>
-          <button>Delete</button>
-        </tr>
-      );
-    })}
-  </tbody>
-</table>
-
-        </div>
-    );
+  return (
+    <div className="Listing">
+      <table>
+        <thead>
+          <tr>
+            <th>File Name</th>
+            <th>File Type</th>
+            <th>Size</th>
+            <th>Date Created</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => {
+            return (
+              <tr key={item.password}>
+                <td>{item.firstname}</td>
+                <td>{item.lastname}</td>
+                <td>{item.password}</td>
+                <td>{item.email}</td>
+                <button>Download</button>
+                <button>Delete</button>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
- 
+
 export default Listing;
